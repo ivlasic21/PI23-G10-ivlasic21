12ea270f9218c8ff2e13dfe7da091bceee52821a
Ivan Vlašić <ivlasic21@foi.hr>
2023-05-16 23:05:50 +0200

    vjezba 4

---
+++ Evaluation Manager___/Models/Evaluation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation_Manager.Models {
+    public class Evaluation { //sadrži objekte drugih klasa ova prva 3 
+
+        public Activity Activity { get; set; }
+        public Student Student { get; set; }
+        public Teacher Evaluator { get; set; }
+        public DateTime EvaluationDate { get; set; }
+        public int Points { get; set; }
+        public bool IsSufficentForGrade() {
+            return Points >= Activity.MinPointsForGrade;
+        }
+        public bool IsSufficientForSigniture() {
+            return Points >= Activity.MinPointsForSignature;
+        }
+    }
+}
